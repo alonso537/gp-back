07af0aef6991d6016e791ced4770b400c18a78a5
Alonso <adrianaalonso002@gmail.com>
2025-02-09 10:24:46 -0600

    acomodar adapter jwt

--- src/config/adapters/jwt.adapter.ts
+++ src/config/adapters/jwt.adapter.ts
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken';
 
 
 export const jwtAdapter = {
-    sign: (payload: any, secret: string, expiresIn: string) => jwt.sign(payload, secret, {expiresIn}),
+    sign: (payload: any, secret: string, expiresIn: string) => jwt.sign(payload, secret, {expiresIn, algorithm: 'HS256'}),
     verify: (token: string, secret: string) => jwt.verify(token, secret)
 }
